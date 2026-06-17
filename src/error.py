98b99bb591569f5a33ace7e8cceed3f612e03f7f
juruan <ruanjun90@gmail.com>
2026-06-17 23:47:21 +0200

    flake8 check

--- src/error.py
+++ src/error.py
@@ -7,11 +7,12 @@
 #   By: junruan <junruan@student.42.fr>              +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/06/09 08:29:49 by junruan             #+#    #+#            #
-#   Updated: 2026/06/09 08:30:21 by junruan            ###   ########.fr      #
+#   Updated: 2026/06/17 23:46:57 by junruan            ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
 """Custom exceptions raised by the Fly-in parser and runtime."""
+
 
 class ParsingError(Exception):
     """Raised when the input map file has invalid syntax."""
